49afbc5984c2491a4634eb5e058636c96a6c2c7a
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2021-05-09 06:35:48 +0700

    fix: url breadcrumb setence error index

--- components/UrlBreadcrumb/index.tsx
+++ components/UrlBreadcrumb/index.tsx
@@ -41,8 +41,10 @@ export default class RouterBreadcrumb extends React.Component<
   mkTitleCase(text: string) {
     const sentence = text.toLowerCase().split(' ');
 
-    for(let i = 0; i< sentence.length; i++){
-      sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    for(let i = 0; i < sentence.length; i++){
+      if (sentence[i].length > 0) {
+        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+      }
     }
     
     return sentence.join(' ');
