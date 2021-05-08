96fff4335b30600144936060d70b97119e302d46
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2021-05-08 20:05:15 +0700

    feat: main layout concept

--- layouts/AppLayout.tsx
+++
@@ -1,9 +0,0 @@
-import RootLayout from '@layouts/RootLayout';
-
-export default function AppLayout({ children }) {
-  return (
-    <RootLayout>
-      {children}
-    </RootLayout>
-  )
-}
---
+++ layouts/AppLayout/index.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+import style from './style.module.scss';
+import RootLayout from '@layouts/RootLayout';
+
+export default function AppLayout({ children }) {
+  const [collapsed, setCollapsed] = useState(true);
+
+  return (
+    <RootLayout>
+      <Layout id={style.root}>
+        <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
+
+        <Layout>
+          <Header id={style.header} />
+          
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+
+            <section id={style.content}>
+              {children}
+            </section>
+          </Content>
+          <Footer id={style.footer}>
+            Next.js Admin ©2021 Created by QaiserLab/Fadlun Anaturdasa Wibawa
+          </Footer>
+        </Layout>
+      </Layout>
+    </RootLayout>
+  )
+}
