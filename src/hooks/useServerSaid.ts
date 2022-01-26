ff6dcc6df591bce2942d3708bf7fd918d5bca4c1
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2022-01-26 07:17:28 +0700

    fix: types dir & config

--- src/hooks/useServerSaid.ts
+++ src/hooks/useServerSaid.ts
@@ -1,12 +1,14 @@
+import TServerSaid from "@types/TServerSaid"
 import { useState } from "react"
 
 export default function useServerSaid() {
-  const SAID_NOTHING = {
+  const SAID_NOTHING:TServerSaid = {
     code: -1,
     message: '',
     errors: {},
   }
-  const [serverSaid, setServerSaid] = useState(SAID_NOTHING)
+
+  const [serverSaid, setServerSaid] = useState<TServerSaid>(SAID_NOTHING)
   const clearServerSaid = () => setServerSaid(SAID_NOTHING)
   
   return {
